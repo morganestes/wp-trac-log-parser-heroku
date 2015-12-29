6d77c46e46d85a75b4d3edf05c08869c7454dfd1
Morgan Estes <morgan.estes@gmail.com>
2015-12-29 15:20:43 -0600

    Provide feedback during the form submission process
    Don't just sit there, let the users know something's happening! In this case, "something"
    is disabling the submit button so we don't go crazy mashing it, add a little fun spinny-thingy,
    and change the text on the button to make it clear something exciting is happening.

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,33 @@
 var express = require('express');
 var router = express.Router();
 
+var tracParser = require('../wp-trac-log-parse');
+var marked = require('marked');
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'WP Trac Log Parser' });
+});
+
+router.post('/', function (req, res) {
+  var newest = req.body['startRevision'];
+  var oldest = parseInt(req.body['stopRevision'], 10);
+  var limit = parseInt(req.body['revisionLimit'], 10);
+
+  tracParser.getLogs(newest, oldest, limit);
+
+  this.setTimeout(function () {
+    console.info('posted to /trac: from %d to %d', oldest, newest);
+    console.dir(process.children, {depth: null, colors: true});
+    var report = tracParser.getReport();
+    console.dir(report, {depth: null, colors: true});
+
+    res.render('trac', {
+      title: 'Logs parsed for revisions %1$s to %2$s'.replace('%1$s', oldest.toString()).replace('%2$s', newest.toString()),
+      log: {html: marked(report), raw: report}
+    });
+    res.end();
+  }, 8000);
 });
 
 module.exports = router;
--- routes/trac.js
+++ routes/trac.js
@@ -1,35 +1,10 @@
 var express = require('express');
 var router = express.Router();
-var tracParser = require('../wp-trac-log-parse');
-var marked = require('marked');
 
-/* GET log page. */
 router.get('/', function (req, res) {
   console.log(req.query);
-  res.render('trac', {
-    title: 'Requesting logs for changesets %1$s to %2$s'.replace('%1$s', req.query.stopRevision).replace('%2$s', req.query.startRevision),
-    logData: 'Please use the form on the homepage to submit queries.'
-  });
-
-}).post('/', function (req, res) {
-  var newest = req.body['startRevision'];
-  var oldest = parseInt(req.body['stopRevision'], 10);
-  var limit = parseInt(req.body['revisionLimit'], 10);
-
-  tracParser.getLogs(newest, oldest, limit);
-
-  this.setTimeout(function () {
-    console.info('posted to /trac: from %d to %d', oldest, newest);
-    console.dir(process.children, {depth: null, colors: true});
-    var report = tracParser.getReport();
-    console.dir(report, {depth: null, colors: true});
-
-    res.render('trac', {
-      title: 'Logs parsed for revisions %1$s to %2$s'.replace('%1$s', oldest.toString()).replace('%2$s', newest.toString()),
-      log: {html: marked(report), raw: report}
-    });
-    res.end();
-  }, 8000);
+  res.redirect('/');
+  next();
 });
 
 module.exports = router;
